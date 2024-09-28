cd9cc86b7b806bd0620c821d28c13fc5325bf932
Anton <panyutin15@gmail.com>
2024-09-28 14:29:44 +0300

    Initial commit

---
+++ src/main/java/com/aproject/carsharing/car_sharing_service/CarSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aproject.carsharing.car_sharing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSharingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aproject/carsharing/car_sharing_service/CarSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aproject.carsharing.car_sharing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
