52b67a0c826c39e23a64b5a009e52f455a37eb7c
Anton <panyutin15@gmail.com>
2024-10-03 22:59:17 +0300

    [Test] added H2 DataBase dependency (V2)

---
+++ src/main/java/com/aproject/carsharing/model/Rental.java
@@ -0,0 +1,44 @@
+package com.aproject.carsharing.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@SQLDelete(sql = "UPDATE rentals SET is_deleted = TRUE WHERE id =?")
+@SQLRestriction(value = "is_deleted = FALSE")
+@Table(name = "rentals")
+public class Rental {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private LocalDate rentalDate;
+    @Column(nullable = false)
+    private LocalDate returnDate;
+    private LocalDate actualReturnDate;
+    @ManyToOne
+    @JoinColumn(name = "car_id", referencedColumnName = "id")
+    private Car car;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+    @ToString.Exclude
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
